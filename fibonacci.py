f7ea3322e06baedc98c74bb28c276d2a30ad3d06
Jharol David <68159039+JharolDev@users.noreply.github.com>
2024-01-04 09:47:56 -0500

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+### Suma de numeros 
+def fibonacci ( n) : 
+// declaracion de numeros 
+  if n <= 1:
+
+           return n
+    else :
+        return fibonacci : ( n -1  )  +  fibonacci ( n – 2 )
